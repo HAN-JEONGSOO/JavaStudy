06dea6a72c1bf69a3b963912fe8aaa8e776d93c8
HAN-JEONGSOO <104303320+HAN-JEONGSOO@users.noreply.github.com>
2022-10-26 22:32:50 +0900

    Create 002_AddTwoIntegers.java

---
+++ 002_AddTwoIntegers.java
@@ -0,0 +1,25 @@
+// 두 정수의 합을 구하는 프로그램
+
+class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("두 개의 숫자를 입력");
+        int first = 10;
+        int second = 20;
+
+        System.out.println(first + " " + second);
+
+        // 두 수를 더한다
+        int sum = first + second;
+        System.out.println("합은: " + sum);
+    }
+}
+
+
+/*
+<Output>
+두 개의 숫자를 입력
+10 20
+합은: 30
+*/
