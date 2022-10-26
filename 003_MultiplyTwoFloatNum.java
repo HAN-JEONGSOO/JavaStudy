1f0a9aea9c8687d7e075ea3e854aa54cc5dc9f43
HAN-JEONGSOO <104303320+HAN-JEONGSOO@users.noreply.github.com>
2022-10-26 23:27:49 +0900

    Create 003_MultiplyTwoFloatNum.java

---
+++ 003_MultiplyTwoFloatNum.java
@@ -0,0 +1,20 @@
+// 두 개의 부동 소수점 숫자의 곱
+
+public class MultiplyTwoNumbers {
+
+    public static void main(String[] args) {
+
+        float first = 1.5f;
+        float second = 2.0f;
+
+        float product = first * second;
+
+        System.out.println("결과는: " + product);
+    }
+}
+
+
+/*
+<Output>
+결과는: 3.0
+*/
