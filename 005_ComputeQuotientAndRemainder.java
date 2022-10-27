d7a7d9b002dcecec163e38af500fcb9ffc82e26a
HAN-JEONGSOO <104303320+HAN-JEONGSOO@users.noreply.github.com>
2022-10-27 10:25:13 +0900

    Create 005_ComputeQuotientAndRemainder.java

---
+++ 005_ComputeQuotientAndRemainder.java
@@ -0,0 +1,22 @@
+// 몫과 나머지 계산하기
+
+public class QuotientRemainder {
+
+    public static void main(String[] args) {
+
+        int dividend = 25, divisor = 4;
+
+        int quotient = dividend / divisor;
+        int remainder = dividend % divisor;
+
+        System.out.println("몫 = " + quotient);
+        System.out.println("나머지 = " + remainder);
+    }
+}
+
+
+/*
+<Output>
+몫 = 6
+나머지 = 1
+*/
