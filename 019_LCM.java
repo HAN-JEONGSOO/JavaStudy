0dbcc5ded9809eaa1a78b1e842a12679e0cde0e0
HAN-JEONGSOO <104303320+HAN-JEONGSOO@users.noreply.github.com>
2022-11-04 12:07:17 +0900

    Update 019_LCM.java

--- 019_LCM.java
+++ 019_LCM.java
@@ -1,5 +1,4 @@
-// LCM : 최소공배수 (Least Common Multiple/ Lowest Common Multiple)
-
+package programizExample;
 import java.util.Scanner;
 
 public class LCM {
@@ -8,6 +7,8 @@ public class LCM {
 		
 		Scanner scanner = new Scanner(System.in);
 		
+		// while문을 써서 최소공배수 구하기
+
 		int num1 = scanner.nextInt();
 		int num2 = scanner.nextInt();
 		int lcm = (num1 > num2) ? num1 : num2;
@@ -22,14 +23,35 @@ public class LCM {
 			}
 			lcm++;
 		}
+		
+		
+		// 최대공약수(GCD)를 사용하여 최소공배수 구하기
+		
+		int num1 = scanner.nextInt();
+		int num2 = scanner.nextInt();
+		int gcd = 1;
+		
+		for (int i = 1; i <= num1 && i <= num2; i++)
+		{
+			if (num1 % i == 0 && num2 % i == 0)
+			{
+				gcd = i;
+				
+			}
+			
+		}
+		
+		int lcm = (num1 * num2) / gcd;
+		System.out.printf("%d와(과) %d의 최소공배수는 %d", num1, num2, lcm);
 
 	}
 
 }
 
+
 /*
 <Output>
-2512
-5737
-2512와(과) 5737의 최소공배수는 14411344
+15
+96
+15와(과) 96의 최소공배수는 480
 */
